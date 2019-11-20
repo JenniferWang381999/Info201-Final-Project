8d9784a4cb7a5c40f3d88023c585f95fd44c8b09
Israel Martinez <israelma@uw.edu>
2019-11-19 19:12:18 -0800

    Fixed title

--- average_means_graph.R
+++ average_means_graph.R
@@ -1,21 +1,26 @@
 library(ggplot2)
-library(reshape2)
+library(tidyr)
 library(lubridate)
 
 # Read the csv and store it under the name "avg_means"
 avg_means <- read.csv("data/seattle_apt_2015_2019.csv",
                       stringsAsFactors = FALSE)
+View(avg_means)
 
-# Creates new dataframe where each bedroom size, month, and price is in one
-# column. This solution was taken from the first answer in this post:
-# https://stackoverflow.com/questions/27382649/a-line-graph-for-each-row
-avg_means2 <- melt(
-  avg_means, id = "Bedroom_Size", na.rm = TRUE, value.name = "price"
+
+# Creates new dataframe with a column for all month dates
+avg_means2 <- avg_means %>%
+  gather(
+    key = months,
+    value = price,
+    -Bedroom_Size
   )
 
-# Removes the "X" and converts the variable column into dates
-avg_means2$variable <- format(
-  as.Date(avg_means2$variable, format = "X%Y.%d"), "%Y-%d")
+
+# Removes the "X" and converts the months column into dates
+avg_means2$months <- format(
+  as.Date(avg_means2$months, format = "X%Y.%d"), "%Y-%d")
+View(avg_means2)
 
 # Return the first and last part of objects in the avg_means2 dataframe.
 head(avg_means2)
@@ -25,8 +30,9 @@ ggplot() +
   # Points are placed the prices of housing for each month, separated by the
   # Bedroom Size.
   geom_point(data = avg_means2,
-            aes(x = variable, y = price, color = Bedroom_Size), group = 3
+            aes(x = months, y = price, color = Bedroom_Size), group = 3
             ) +
+  ggtitle("Seattle Apartment Prices 2015-2019") +
   # Turns the x-axis labels to 45 degrees so that it is readable
   # This solution was taken from the second answer in this post:
   # https://stackoverflow.com/questions/50399838/how-to-alternate-a-new-line
